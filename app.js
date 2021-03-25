550a4125dcfe5b82d06724df0069199b540cc495
roi.kramer <roi.kramer@codefresh.io>
2021-03-25 11:04:05 +0200

    wip

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require('cors');
+const morgan = require('morgan');
 const Packages = require('./data/packages');
+
 
 const packages = new Packages();
 
@@ -14,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
